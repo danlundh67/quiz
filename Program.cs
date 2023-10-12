6293aa0d76d06d83256da9842744d1fabb7f30a3
Dan Lund <dan.lundh@outlook.com>
2023-10-12 10:15:51 +0200

    initial commit 	new file:   Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace quiz
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Quiz m1 = new Quiz("What is the capital of United Kingdom?");
+            
+            Stringanswer m2 = new Stringanswer("London", "State the capital of United Kingdom");
+           
+            // Stringanswer m11 = new Stringanswer
+            
+
+
+            Console.WriteLine(m2.Thequerytext + " " + m2.correct);
+        }
+    }
+}
