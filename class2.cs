5f14dc501d178bd787338bd43ee9799c09383564
Richard Bodén <utjis@hotmail.com>
2023-10-17 09:34:39 +0200

    Correct methods in class 2

--- class2.cs
+++ class2.cs
@@ -7,8 +7,6 @@ public class YearQuestion : Quiz
         this.year = year;
         
     }
-
-    
     public override void CorrectAnswer()
     {
         System.Console.WriteLine(Thequerytext);
@@ -34,17 +32,68 @@ class TrueOrFalseQuestion : Quiz
     {
         this.IsTrue = IsTrue;
     }
-
-    
+public override void CorrectAnswer()
+    {
+        System.Console.WriteLine(Thequerytext);
+        System.Console.WriteLine("What is your answer (true/false)");
+        string boolAnswer = "" + Console.ReadLine();
+        if (bool.TryParse(boolAnswer, out bool answer))
+        {    
+            if (answer == IsTrue)
+            {
+                Console.WriteLine("Correct answer");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect answer");
+                Points = 0;
+            }
+        }else
+        {
+            System.Console.WriteLine("Invalid answer. Please enter 'true' or 'false'.");
+            Points = 0;
+        }
+        
+    }
 }
 public class MultipleChoises : Quiz
 {
-    List<MultipleChoises> Choices = new List<MultipleChoises>();
-    public string choice;
+    List<string> Choices = new List<string>();
+    public string Correctchoice;
 
-    public MultipleChoises (string choice, string thequerytext, int points) : base(thequerytext, points)
+    public MultipleChoises (string thequerytext, int points, List<string> choices) : base(thequerytext, points)
     {
-        this.choice = choice;
+        this.Choices = choices;
     }
+    public void SetCorrectChoice(string choice)
+    {
+        Correctchoice = choice;
+        
+    }
+    public override void CorrectAnswer()
+    {
+        System.Console.WriteLine(Thequerytext);
+        System.Console.WriteLine("What is your answer. There can only be one right answer!");
+        string answer = "" + Console.ReadLine();
+        if (Choices.Contains(answer))
+        {
+            if (answer == Correctchoice)
+            {
+                Console.WriteLine("Correct answer");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect answer");
+                Points = 0;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid choice. Please choose one of the available options.");
+            Points = 0;
+        }
+        
+    }
+
 }
 
