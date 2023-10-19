1deb006efc2f52ce96a7623a96afae0ba5f82623
Richard Bodén <utjis@hotmail.com>
2023-10-19 08:49:21 +0200

    removed function not in use

--- class.cs
+++ class.cs
@@ -24,36 +24,5 @@ public class Quiz
     }
     
 
-    public void DoSomething(string correct)
-    {
-        Console.WriteLine(Thequerytext);
-        string answer = "" +Console.ReadLine();
-        if (answer == correct)
-        {
-            Console.WriteLine("Correct answer");
-        }
-        else
-        {
-            Console.WriteLine("Incorrect answer");
-            System.Console.WriteLine("----------------");
-        }
-    }
     
-    public void DoSomething(bool correct)
-    {
-         Console.WriteLine(Thequerytext);
-        string answer = "" +Console.ReadLine();
-        bool.TryParse(answer.ToLower(), out bool result);
-
-        if (result == correct)
-        {
-            Console.WriteLine("Correct answer");
-            System.Console.WriteLine("----------------");
-        }
-        else
-        {
-            Console.WriteLine("Incorrect answer");
-            System.Console.WriteLine("----------------");
-        }
-    }
 }
