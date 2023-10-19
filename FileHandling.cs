0cd572e0a36587b46a0992e5a1f0760e9cb5c55f
Dan Lund <dan.lundh@outlook.com>
2023-10-19 10:17:58 +0200

    filehandle

--- FileHandling.cs
+++ FileHandling.cs
@@ -5,15 +5,18 @@ class FileHandling
 
     public static void SaveToFile(List<Quiz> myquiz, string Filename)
     {
+
+            StreamWriter sw = new StreamWriter(Filename);
             foreach(Quiz item in myquiz)
             {
 
                 if(item is Stringanswer child1)
                 {
-                   Console.WriteLine("Stringanswer",child1.thequerytext, child1.Points, child1.correct);
+                   sw.WriteLine("Stringanswer",child1.thequerytext, child1.Points, child1.correct);
                 }
 
             }
+            sw.Close();
 
         string strJson = JsonSerializer.Serialize(myquiz, new JsonSerializerOptions { WriteIndented = true });
         File.WriteAllText(Filename, strJson);
