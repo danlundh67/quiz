34daa04cc9c84c6b54c03e434c0a441adc2beaec
Dan Lund <dan.lundh@outlook.com>
2023-10-19 10:42:57 +0200

    Filehandling

--- FileHandling.cs
+++ FileHandling.cs
@@ -12,7 +12,7 @@ class FileHandling
 
                 if(item is Stringanswer child1)
                 {
-                   sw.WriteLine("Stringanswer",child1.thequerytext, child1.Points, child1.correct);
+                   sw.WriteLine("Stringanswer;",child1.thequerytext+";", child1.Points +";", child1.correct);
                 }
 
             }
@@ -24,8 +24,36 @@ class FileHandling
 
     public static List<Quiz> ReadFromFile(string Filename)
     {
-        string strJson = File.ReadAllText(Filename);
-        return JsonSerializer.Deserialize<List<Quiz>>(strJson); 
+
+         StreamReader sr = new StreamReader(Filename);
+         List<Quiz> tmp =new List<Quiz>();
+        //Read the first line of text
+        string line = sr.ReadLine();
+        //Continue to read until you reach end of file
+        while (line != null)
+        {
+            //write the line to console window
+            Console.WriteLine(line);
+
+            // split på line (rad)
+            string[] words = line.Split(';');
+            // om 1:a texten är Stringanswer - skapa stringanswer objekt
+
+            if (words[0] == "Stringanswer")
+            {
+
+                Stringanswer myst = new Stringanswer(words[3],words[1],int.Parse(words[2]));
+                tmp.Add(myst);
+            }
+
+            //Read the next line
+            line = sr.ReadLine();
+        }
+        //close the file
+        sr.Close();
+
+        
+        return tmp; 
     }
     
 }
