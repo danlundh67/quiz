22954b5ce9d9b72f8e57114743ae5dbea9397e35
Dan Lund <dan.lundh@outlook.com>
2023-10-19 10:12:49 +0200

    ex på filesparning

--- FileHandling.cs
+++ FileHandling.cs
@@ -5,6 +5,16 @@ class FileHandling
 
     public static void SaveToFile(List<Quiz> myquiz, string Filename)
     {
+            foreach(Quiz item in myquiz)
+            {
+
+                if(item is Stringanswer child1)
+                {
+                   Console.WriteLine("Stringanswer",child1.thequerytext, child1.Points, child1.correct);
+                }
+
+            }
+
         string strJson = JsonSerializer.Serialize(myquiz, new JsonSerializerOptions { WriteIndented = true });
         File.WriteAllText(Filename, strJson);
     }
